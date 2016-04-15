cf4db0d3786b0fc3983a791652b3a1ae8456ae91
Chris Duranti <chrisd1891@gmail.com>
2016-04-14 22:55:23 -0700

    ..

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Usic.Mixfile do
     [
       {:phoenix, "~> 1.0.3"},
       {:phoenix_ecto, "~> 1.1"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.11.0"},
       {:phoenix_html, "~> 2.1"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
